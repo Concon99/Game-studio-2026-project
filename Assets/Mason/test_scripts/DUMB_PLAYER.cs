edd2ccc976ce821aeb51b9cda5f3fa84b7304bfa
ornifer <imnotthemano@gmail.com>
2026-03-03 16:03:22 -1000

    start base

---
+++ Assets/Mason/test_scripts/DUMB_PLAYER.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DUMB_PLAYER : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
